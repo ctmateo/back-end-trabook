2f63b660f97766415e265c4166a6997298f7a8e2
Mateo <eliermateoramirez@gmail.com>
2023-11-19 16:07:16 -0500

    solve server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } = require('./config.js
 
 
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -12,18 +12,23 @@ const port = parseInt(DB_PORT, 10);
 app.use(cors());
 app.use(express.json());
 
-const connection = mysql.createConnection({
+
+
+const pool = mysql.createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10, // Ajusta según tus necesidades
+  queueLimit: 0,
 });
 
-connection.connect();
+pool.connect();
 
 app.get('/', (req,res)=>{
   const consulta = 'SELECT * FROM cards';
-  connection.query(consulta, (error,resultados) =>  {
+  pool.query(consulta, (error,resultados) =>  {
     if(error) throw error;
     res.json(resultados);
   });
@@ -31,7 +36,7 @@ app.get('/', (req,res)=>{
 
 app.get('/bestvacation', (req,res)=>{
   const consulta = 'SELECT * FROM cards_bestvacation';
-  connection.query(consulta, (error,resultados) =>  {
+  pool.query(consulta, (error,resultados) =>  {
     if(error) throw error;
     res.json(resultados);
   });
@@ -39,7 +44,7 @@ app.get('/bestvacation', (req,res)=>{
 
 app.get('/blog', (req,res)=>{
   const consulta = 'SELECT * FROM cards_blog';
-  connection.query(consulta, (error,resultados) =>  {
+  pool.query(consulta, (error,resultados) =>  {
     if(error) throw error;
     res.json(resultados);
   });

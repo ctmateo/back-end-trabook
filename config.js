b87c66b882c1c359bb812ceb2fbf1be70ec8066c
Mateo <eliermateoramirez@gmail.com>
2023-11-19 15:01:32 -0500

    upgrade back-end

---
+++ config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  DB_HOST: process.env.DB_HOST || '127.0.0.1',
+  DB_USER: process.env.DB_USER || 'root',
+  DB_PASSWORD: process.env.DB_PASSWORD || 'Mattstore01.',
+  DB_NAME: process.env.DB_NAME || 'trabook_db',
+  DB_PORT: 3006
+};
---
+++ server.js
@@ -0,0 +1,49 @@
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } = require('./config.js');
+
+
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+const port = DB_PORT;
+
+app.use(cors());
+app.use(express.json());
+
+const connection = mysql.createConnection({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+});
+
+connection.connect();
+
+app.get('/', (req,res)=>{
+  const consulta = 'SELECT * FROM cards';
+  connection.query(consulta, (error,resultados) =>  {
+    if(error) throw error;
+    res.json(resultados);
+  });
+});
+
+app.get('/bestvacation', (req,res)=>{
+  const consulta = 'SELECT * FROM cards_bestvacation';
+  connection.query(consulta, (error,resultados) =>  {
+    if(error) throw error;
+    res.json(resultados);
+  });
+});
+
+app.get('/blog', (req,res)=>{
+  const consulta = 'SELECT * FROM cards_blog';
+  connection.query(consulta, (error,resultados) =>  {
+    if(error) throw error;
+    res.json(resultados);
+  });
+});
+
+app.listen(port, () =>{
+  console.log('corriendo en ',DB_PORT)
+})

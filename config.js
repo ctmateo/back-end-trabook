3bd6a62a0f029a7fb0507e7e494a8b7669e5a2b5
Mateo <eliermateoramirez@gmail.com>
2023-11-19 15:45:40 -0500

    change port

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ module.exports = {
   DB_USER: process.env.DB_USER || 'root',
   DB_PASSWORD: process.env.DB_PASSWORD || 'Mattstore01.',
   DB_NAME: process.env.DB_NAME || 'trabook_db',
-  DB_PORT: 3006
+  DB_PORT: process.env.DB_PORT || 3306
 };

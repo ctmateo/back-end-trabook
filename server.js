132f856e8208168e27a557916de18bb08444edf2
Mateo <eliermateoramirez@gmail.com>
2023-11-19 16:27:51 -0500

    solve server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } = require('./config.js');
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -10,59 +10,46 @@ const port = parseInt(DB_PORT, 10);
 app.use(cors());
 app.use(express.json());
 
-const connection = mysql.createConnection({
+// Crea un pool de conexiones
+const pool = mysql.createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
-  connectTimeout: 20000,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-// Manejo de errores de conexión a la base de datos
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err);
-  } else {
-    console.log('Connected to the database!');
-  }
-});
+// No es necesario pool.connect() en este caso
 
 app.get('/', (req, res) => {
   const consulta = 'SELECT * FROM cards';
-  connection.query(consulta, (error, resultados) => {
-    if (error) {
-      console.error('Error executing query:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    } else {
-      res.json(resultados);
-    }
+  // Utiliza el pool para ejecutar consultas
+  pool.query(consulta, (error, resultados) => {
+    if (error) throw error;
+    res.json(resultados);
   });
 });
 
 app.get('/bestvacation', (req, res) => {
   const consulta = 'SELECT * FROM cards_bestvacation';
-  connection.query(consulta, (error, resultados) => {
-    if (error) {
-      console.error('Error executing query:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    } else {
-      res.json(resultados);
-    }
+  // Utiliza el pool para ejecutar consultas
+  pool.query(consulta, (error, resultados) => {
+    if (error) throw error;
+    res.json(resultados);
   });
 });
 
 app.get('/blog', (req, res) => {
   const consulta = 'SELECT * FROM cards_blog';
-  connection.query(consulta, (error, resultados) => {
-    if (error) {
-      console.error('Error executing query:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    } else {
-      res.json(resultados);
-    }
+  // Utiliza el pool para ejecutar consultas
+  pool.query(consulta, (error, resultados) => {
+    if (error) throw error;
+    res.json(resultados);
   });
 });
 
 app.listen(port, () => {
-  console.log('Server is running on port', port);
+  console.log('corriendo en ', DB_PORT);
 });

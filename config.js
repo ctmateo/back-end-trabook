e1fa49dbc798abbecab09a355de26110bd9278a2
Mateo <eliermateoramirez@gmail.com>
2023-11-19 19:18:06 -0500

    connect to BD cloud

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 
 module.exports = {
-  DATABASE_URL:'mysql://64dfqy2adzfjfserzbdy:pscale_pw_FhAOvDdPRBTaZYBiwZvHdoNZVhUeia4nRTCaYYm1s14@aws.connect.psdb.cloud/trabook_db?ssl={"rejectUnauthorized":true}'
+  DATABASE_URL:'mysql://cnku8p3w5xebkbnvzckf:pscale_pw_9i2wf0eUvzMV4bkmO4g5gs8m8uelbGecJGDHMdaiw4W@aws.connect.psdb.cloud/trabook_db?ssl={"rejectUnauthorized":true}'
+
 };

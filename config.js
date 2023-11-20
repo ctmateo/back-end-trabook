61ec0ac96116c4ed02dca23a36d4c93f6830f19d
Mateo <eliermateoramirez@gmail.com>
2023-11-19 19:13:21 -0500

    connect to BD cloud

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
 module.exports = {
-  DATABASE_URL: 'mysql://h0xlcoqb6emu7qo48bq1:pscale_pw_9M5yMyn5kjwvZnpA7S34vOdSHtJrou3NgxdyTYybm4p@aws.connect.psdb.cloud/trabook_db?ssl={"rejectUnauthorized":true}'
+  DATABASE_URL:'mysql://64dfqy2adzfjfserzbdy:pscale_pw_FhAOvDdPRBTaZYBiwZvHdoNZVhUeia4nRTCaYYm1s14@aws.connect.psdb.cloud/trabook_db?ssl={"rejectUnauthorized":true}'
 };
--- server.js
+++ server.js
@@ -12,42 +12,37 @@ app.use(express.json());
 const url = require('url');
 const connectionData = url.parse(DATABASE_URL);
 
-// Configuración de SSL
+
 const sslQueryParam = new URL(DATABASE_URL).searchParams.get('ssl');
 const ssl = sslQueryParam ? JSON.parse(sslQueryParam) : null;
 
 
-
-// Obtener el nombre de usuario de la propiedad 'auth' de la URL
 const auth = connectionData.auth ? `${connectionData.auth}@` : '';
 const connectionConfig = {
   host: connectionData.hostname,
-  user: connectionData.auth.split(':')[0],  // Obtener el nombre de usuario
-  password: connectionData.auth.split(':')[1],  // Obtener la contraseña
+  user: connectionData.auth.split(':')[0],  
+  password: connectionData.auth.split(':')[1],  
   database: connectionData.pathname.replace('/', ''),
   port: connectionData.port,
   ssl: ssl,
 };
 
-// Creación del pool de conexión
 const pool = mysql.createPool({
   ...connectionConfig,
-  // Añade la siguiente línea para habilitar la conexión segura con SSL/TLS
   ssl: ssl ? { ...ssl, rejectUnauthorized: false } : null,
 });
 
-// Obtención de conexión desde el pool
+
 pool.getConnection((err, connection) => {
   if (err) {
     console.error('Error connecting to the database:', err);
   } else {
     console.log('Connected to the database!');
-    // Luego de conectar, puedes liberar la conexión
     connection.release();
   }
 });
 
-// Rutas de la aplicación
+
 app.get('/', (req, res) => {
   const consulta = 'SELECT * FROM cards';
   pool.query(consulta, (error, resultados) => {
@@ -72,7 +67,7 @@ app.get('/blog', (req, res) => {
   });
 });
 
-// Inicio del servidor
+
 app.listen(port, "0.0.0.0", () => {
   console.log('Ejecutado en el puerto ', port);
 });
